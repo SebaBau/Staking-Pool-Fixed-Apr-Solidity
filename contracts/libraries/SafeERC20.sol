a7fcfce8cc02555c84f40d5652e040bcab73b298
Sebastian Bauer <sebastian.bauer9393@gmail.com>
2023-01-13 21:16:27 +0100

    LIBRARY - ADD : Added 'SafeERC20' library with custom implemented logic

---
+++ contracts/libraries/SafeERC20.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+
+import "../interfaces/IERC20.sol";
+
+pragma solidity 0.8.17;
+
+library SafeERC20 {
+    error SafeERC20_OnlyContractAllowed();
+    error SafeERC20_TransferFailed();
+
+    function safeTransfer(
+        address token,
+        address to,
+        uint256 amount
+    ) internal {
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(IERC20.transfer.selector, to, amount));
+        _validateTransfer(token, success, data);
+    }
+
+    function safeTransferFrom(
+        address token,
+        address from,
+        address to,
+        uint256 amount
+    ) internal returns (uint256) {
+        uint256 preTransferBalance = IERC20(token).balanceOf(to);
+        (bool success, bytes memory data) = token.call(
+            abi.encodeWithSelector(IERC20.transferFrom.selector, from, to, amount)
+        );
+        _validateTransfer(token, success, data);
+        uint256 postTransferBalance = IERC20(token).balanceOf(to);
+
+        return postTransferBalance - preTransferBalance;
+    }
+
+    function _validateTransfer(
+        address token,
+        bool success,
+        bytes memory data
+    ) private view {
+        if (success) {
+            if (data.length == 0) {
+                if (token.code.length == 0) revert SafeERC20_OnlyContractAllowed();
+            } else {
+                if (!abi.decode(data, (bool))) revert SafeERC20_TransferFailed();
+            }
+        } else {
+            revert SafeERC20_TransferFailed();
+        }
+    }
+}
