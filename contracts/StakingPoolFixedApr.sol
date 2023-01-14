90eafedd4dcc0701c415590acd451dda9b121633
Sebastian Bauer <sebastian.bauer9393@gmail.com>
2023-01-14 22:24:49 +0100

    CONTRACT - EDIT : Added validation for input data in the 'addStakingPool' function (new private '_validateStakingPoolData' function), added 'minimumToStake' field in the 'StakingPool' struct and added setting this field and in event in the 'StakingPoolsFixedApr' contract

--- contracts/StakingPoolFixedApr.sol
+++ contracts/StakingPoolFixedApr.sol
@@ -12,14 +12,16 @@ contract StakingPoolFixedApr is Ownable {
     using SafeERC20 for IERC20;
 
     error StakingPoolFixedApr_IncorrectAmountTransferred();
+    error StakingPoolFixedApr_ZeroRewardsAmount();
+    error StakingPoolFixedApr_StartTimeMustBeInTheFuture();
+    error StakingPoolFixedApr_StartTimeMustBeLaterThanEndTime();
 
     struct StakingPool {
         uint256 rewardsAdded;
+        uint256 minimumToStake;
         IERC20 token;
         uint64 startTime;
-        uint32 unstakePeriod;
         uint64 endTime;
-        uint16 penaltyFee;
         uint16 apr;
     }
 
@@ -30,7 +32,8 @@ contract StakingPoolFixedApr is Ownable {
 
     event StakingPoolAdded(
         uint256 indexed stakingPoolId,
-        uint256 rewards,
+        uint256 rewardsAdded,
+        uint256 minimumToStake,
         address token,
         uint64 startTime,
         uint64 endTime,
@@ -38,7 +41,8 @@ contract StakingPoolFixedApr is Ownable {
     );
 
     function addStakingPool(
-        uint256 rewards,
+        uint256 rewardsAmount,
+        uint256 minimumToStake_,
         IERC20 token_,
         uint64 startTime_,
         uint64 endTime_,
@@ -46,17 +50,28 @@ contract StakingPoolFixedApr is Ownable {
     ) external onlyOwner {
         uint256 stakingPoolId = ++lastStakingPoolId;
 
+        _validateStakingPoolData(rewardsAmount, startTime_, endTime_);
+
         StakingPool storage stakingPool = stakingPools[stakingPoolId];
-        stakingPool.rewardsAdded = rewards;
+        stakingPool.rewardsAdded = rewardsAmount;
+        stakingPool.minimumToStake = minimumToStake_;
         stakingPool.token = token_;
         stakingPool.startTime = startTime_;
         stakingPool.endTime = endTime_;
         stakingPool.apr = apr_;
 
-        if (rewards != token_.safeTransferFrom(msg.sender, address(this), rewards))
+        if (rewardsAmount != token_.safeTransferFrom(msg.sender, address(this), rewardsAmount))
             revert StakingPoolFixedApr_IncorrectAmountTransferred();
 
-        emit StakingPoolAdded(stakingPoolId, rewards, address(token_), startTime_, endTime_, apr_);
+        emit StakingPoolAdded(
+            stakingPoolId,
+            rewardsAmount,
+            minimumToStake_,
+            address(token_),
+            startTime_,
+            endTime_,
+            apr_
+        );
     }
 
     function stake() external {}
@@ -66,4 +81,16 @@ contract StakingPoolFixedApr is Ownable {
     function getAllStakingPools() external view returns (StakingPool[] memory stakingPools) {}
 
     function getAllActiveStakingPools() external view returns (StakingPool[] memory stakingPools) {}
+
+    function _validateStakingPoolData(
+        uint256 rewardsAmount,
+        uint64 startTime,
+        uint64 endTime
+    ) private view {
+        if (rewardsAmount == 0) revert StakingPoolFixedApr_ZeroRewardsAmount();
+
+        if (startTime < block.timestamp) revert StakingPoolFixedApr_StartTimeMustBeInTheFuture();
+
+        if (startTime >= endTime) revert StakingPoolFixedApr_StartTimeMustBeLaterThanEndTime();
+    }
 }
