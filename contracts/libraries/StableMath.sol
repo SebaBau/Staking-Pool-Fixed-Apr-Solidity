48d7ec03cab709b65c0b4ad72c594ec4ddb2f9f4
Sebastian Bauer <sebastian.bauer9393@gmail.com>
2023-01-15 20:15:20 +0100

    LIBRARY - ADD : Added fully implemented 'StableMath' library.

---
+++ contracts/libraries/StableMath.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+library StableMath {
+    /**
+     * @dev Scaling unit for use in specific calculations,
+     * where 1 * 10**18, or 1e18 represents a unit '1'
+     */
+    uint256 private constant FULL_SCALE = 1e18;
+
+    /***************************************
+              PRECISE ARITHMETIC
+    ****************************************/
+
+    /**
+     * @dev Multiplies two precise units, and then truncates by the full scale
+     * @param x     Left hand input to multiplication
+     * @param y     Right hand input to multiplication
+     * @return      Result after multiplying the two inputs and then dividing by the shared
+     *              scale unit
+     */
+    function mulTruncate(uint256 x, uint256 y) internal pure returns (uint256) {
+        return (x * y) / FULL_SCALE;
+    }
+
+    /**
+     * @dev Precisely divides two units, by first scaling the left hand operand. Useful
+     *      for finding percentage weightings, i.e. 8e18/10e18 = 80% (or 8e17)
+     * @param x     Left hand input to division
+     * @param y     Right hand input to division
+     * @return      Result after multiplying the left operand by the scale, and
+     *              executing the division on the right hand input.
+     */
+    function divPrecisely(uint256 x, uint256 y) internal pure returns (uint256) {
+        // e.g. 8e18 * 1e18 = 8e36
+        // e.g. 8e36 / 10e18 = 8e17
+        return (x * FULL_SCALE) / y;
+    }
+}
