16205fb518dfebe59a8d684b8f4da7b27cf56284
Sebastian Bauer <sebastian.bauer9393@gmail.com>
2023-01-21 18:31:23 +0100

    CONFIG - EDIT : Added migration from 'hardhat-waffle' to Hardhat Chai Matcfhers and Hardhat Network Helpers

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,8 @@ import { HardhatUserConfig } from "hardhat/config";
 import "./tasks/accounts";
 import "./tasks/clean";
 
-import "@nomiclabs/hardhat-waffle";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-etherscan";
 import "@typechain/hardhat";
 import "solidity-coverage";
